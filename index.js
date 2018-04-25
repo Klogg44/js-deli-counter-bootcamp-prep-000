b3091f00c6ed9b30815492b5bd0c700ed0d4766d
Spencer Croy <spencerccroy@gmail.com>
2018-04-25 22:04:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    let upNow = katzDeliLine[0]
+    katzDeliLine.shift()
+    return `Currently serving ${upNow}.`
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    let string = "The line is currently: ";
+    for (let i=0; i<katzDeliLine.length; i++) {
+      let position = (i + 1) + ". " + katzDeliLine[i] + ", ";
+      string += position;
+    }
+    return string
+  } else {
+    return "The line is currently empty.";
+  }
+}
